1099100514dad30e9183300ac16d066185969bc7
Soren Basnet <basnetsoren01@gmail.com>
2026-03-03 08:43:19 -0600

    mean.sh

--- mean.sh
+++ mean.sh
@@ -1,1 +1,24 @@
 #/bin/bash
+
+
+if [[ $1 == "" || $2 == "" ]]; then
+	echo "Error : mean.sh requires 2 arguments. One or more are missing."
+	exit 1
+fi
+
+column=$1
+file=$2
+
+awk -F ',' -v col="$1" '
+    NR > 1 && $col != "" {
+        sum += $col
+        count++
+    }
+    END {
+        if (count > 0) 
+            print (sum / count)
+        else 
+            exit 1
+    }
+' "${file:-/dev/stdin}"
+
