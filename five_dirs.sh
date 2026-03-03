e31f44ff86c43efa0e455a315161f9346f7c7b37
Wanyun <wsi7@wisc.edu>
2026-03-03 08:35:43 -0600

    completed five_dirs.sh

--- five_dirs.sh
+++ five_dirs.sh
@@ -1,1 +1,17 @@
 #/bin/bash
+
+rm -rf five
+mkdir -p five
+
+for i in {1..5}; do
+  dir="five/dir$i"
+  mkdir -p "$dir"
+
+  for j in {1..4}; do
+    file="$dir/file$j"
+    : > "$file"
+    for k in $(seq 1 "$j"); do
+      echo "$j" >> "$file"
+    done
+  done
+done
