c08db3ea5f345aba9c18c4119d0e348866782c93
Zeyu Huang <zhuang685@wisc.edu>
2026-03-03 08:48:26 -0600

    Add rm_n.sh

--- rm_n.sh
+++ rm_n.sh
@@ -1,1 +1,16 @@
-#/bin/bash
+#!/bin/bash
+
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 <directory> <n_bytes>" 1>&2
+    exit 1
+fi
+
+DIR=$1
+N=$2
+
+if [ ! -d "$DIR" ]; then
+    echo "Error: $DIR is not a directory" 1>&2
+    exit 1
+fi
+
+find "$DIR" -type f -size +"${N}c" -exec rm {} \;
